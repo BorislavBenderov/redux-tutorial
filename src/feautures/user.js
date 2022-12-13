0d8bedd5daa1c56a9b4512ea7ba4f0d0bc914255
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-13 12:52:02 +0200

    Add login and logout reducers

--- src/feautures/user.js
+++ src/feautures/user.js
@@ -1,13 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialStateValue = { name: '', age: 0, email: '' };
+
 export const userSlice = createSlice({
     name: 'user',
-    initialState: { value: { name: '', age: 0, email: '' } },
+    initialState: { value: initialStateValue },
     reducers: {
         login: (state, action) => {
             state.value = action.payload;
+        },
+        logout: (state, action) => {
+            state.value = initialStateValue;
         }
     }
-})
+});
 
+export const { login, logout } = userSlice.actions;
 export default userSlice.reducer;
