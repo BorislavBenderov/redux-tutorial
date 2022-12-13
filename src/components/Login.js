8c2b818f246e5a4d383bb8cb0800527dbf2279fe
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-13 12:53:03 +0200

    Use useDispatch

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,19 @@
+import { useDispatch } from "react-redux";
+import { login, logout } from "../feautures/user";
+
 export const Login = () => {
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <button>Login</button>
+            <button
+                onClick={() => {
+                    dispatch(login({ name: 'Borko', age: 0, email: '' }))
+                }}>Login</button>
+            <button
+                onClick={() => {
+                    dispatch(logout())
+                }}>Logout</button>
         </div>
     );
 }
