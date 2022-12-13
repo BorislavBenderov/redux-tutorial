b2dacd3aef217f71d7ff55387530c7fe753fbd00
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-13 13:26:31 +0200

    useSelector for theme

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,8 +2,10 @@ import { useSelector } from "react-redux";
 
 export const Profile = () => {
     const user = useSelector((state) => state.user.value);
+    const theme = useSelector((state) => state.theme.value)
+
     return (
-        <div>
+        <div style={{ color: theme }}>
             <h1>Profile Page</h1>
             <p>Name: {user.name}</p>
             <p>Age: {user.age}</p>
