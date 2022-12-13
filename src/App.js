40c8ac0df8a8a19932977dca17979aa5c3017caa
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-13 13:25:12 +0200

    Render change color component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Login } from "./components/Login";
 import { Profile } from "./components/Profile";
+import { ChangeColor } from "./components/ChangeColor";
 
 function App() {
   return (
     <div className="App">
       <Profile />
       <Login />
+      <ChangeColor />
     </div>
   );
 }
