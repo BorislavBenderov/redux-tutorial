a3af651833c5d973b5a144482fdc8bb51c51efaf
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-13 12:27:34 +0200

    Import configureStore and Provider from Redux and wrap App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+    reducer: {}
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
