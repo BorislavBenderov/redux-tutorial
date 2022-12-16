8d104d2890d289fef421b6bfab4750dd0f55bfe3
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-16 10:36:06 +0200

    Render totals

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import { Profile } from "./components/Profile";
 import { ChangeColor } from "./components/ChangeColor";
 import { Navbar } from "./components/NavBar";
 import { CartContainer } from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./feautures/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <div className="App">
       <Navbar />
