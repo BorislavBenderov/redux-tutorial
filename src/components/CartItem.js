efe14e427603a77d52f7e5ed71b25f0a88dc5bbc
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-16 09:35:28 +0200

    Render cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,23 @@
+import { ChevronDown, ChevronUp } from '../icons';
+
 export const CartItem = ({item}) => {
     return (
-        <div>{item.id}</div>
+        <article className="cart-item">
+            <img src={item.img} alt={item.title} />
+            <div>
+                <h4>{item.title}</h4>
+                <h4 className="item-price">${item.price}</h4>
+                <button className="remove-btn">remove</button>
+            </div>
+            <div>
+                <button className="amount-btn">
+                    <ChevronUp />
+                </button>
+                <p className="amount">{item.amount}</p>
+                <button className="amount-btn">
+                    <ChevronDown />
+                </button>
+            </div>
+        </article>
     );
 }
