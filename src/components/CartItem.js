6d35cdf4b3cd2e0cfcaa79258b58214d2666fc41
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-16 10:00:52 +0200

    Use remove, increase and decrease actions

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,20 +1,23 @@
 import { ChevronDown, ChevronUp } from '../icons';
+import { useDispatch } from 'react-redux';
+import { removeItem, increase, decrease } from '../feautures/cart/cartSlice';
 
 export const CartItem = ({item}) => {
+    const dispatch = useDispatch();
     return (
         <article className="cart-item">
             <img src={item.img} alt={item.title} />
             <div>
                 <h4>{item.title}</h4>
                 <h4 className="item-price">${item.price}</h4>
-                <button className="remove-btn">remove</button>
+                <button className="remove-btn" onClick={() => dispatch(removeItem(item.id))}>remove</button>
             </div>
             <div>
-                <button className="amount-btn">
+                <button className="amount-btn" onClick={() => dispatch(increase(item.id))}>
                     <ChevronUp />
                 </button>
                 <p className="amount">{item.amount}</p>
-                <button className="amount-btn">
+                <button className="amount-btn" onClick={() => dispatch(decrease(item.id))}>
                     <ChevronDown />
                 </button>
             </div>
