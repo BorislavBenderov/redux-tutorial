8c2f6cae96ed9ea49f3c23a0638fb66357d3a8dc
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-16 09:35:13 +0200

    Add clear cart action

--- src/feautures/cart/cartSlice.js
+++ src/feautures/cart/cartSlice.js
@@ -1,15 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
 
 const initialState = {
-    cartItems: [],
-    amount: 0,
+    cartItems: cartItems,
+    amount: 5,
     total: 0,
     isLoading: true
 };
 
 const cartSlice = createSlice({
     name: 'cart',
-    initialState
+    initialState,
+    reducers: {
+        clearCart:(state) => {
+            state.cartItems = [];
+        }
+    }
 })
+
+export const { clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
