fd0d32b88ad480e09e460894c985e02f5c14aeb4
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-16 09:19:57 +0200

    Add cart container and cart item components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Login } from "./components/Login";
 import { Profile } from "./components/Profile";
 import { ChangeColor } from "./components/ChangeColor";
 import { Navbar } from "./components/NavBar";
+import { CartContainer } from "./components/CartContainer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CartContainer />
     </div>
   );
 }
---
+++ src/components/CartItem.js
@@ -0,0 +1,5 @@
+export const CartItem = ({item}) => {
+    return (
+        <div>{item.id}</div>
+    );
+}
